8f79d542387d5050ac25fe3147e10eb1e0a4bd43
Patricia-1996 <patricia.escobar@edu.pucrs.br>
2026-05-07 14:11:01 +0000

    "banner"

---
+++ bc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int scale = 0;
+
+void mostrar_banner(){
+    printf("bc 1.07.1\n");
+    printf("Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006, 2008, 2012-2017 Free Software Foundation, Inc.");
+    printf("This is free software with ABSOLUTELY NO WARRANTY".\n);
+    printf("For details type 'warranty'. \n");
+}
+
+int main(){
+
+}
+
